e300f3263f221817a016ef5bba0f1cc5ae85eff4
Sergii <sergiigrytsaienko@gmail.com>
2023-09-19 18:32:58 -0500

    test: update test cases for improved coverage

--- src/ng-ag-grid-schematics/tests/index_spec.ts
+++ src/ng-ag-grid-schematics/tests/index_spec.ts
@@ -3,15 +3,35 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../../collection.json');
+import * as angularJsonStub from './stubs/angular.json';
+import * as packageJsonStub from './stubs/package.json';
 
 describe('ng-ag-grid-schematics', () => {
-  it('works', async () => {
+  let testTree: Tree;
+  beforeEach(() => {
+    testTree = Tree.empty();
+    testTree.create('./angular.json', JSON.stringify(angularJsonStub));
+    testTree.create('./package.json', JSON.stringify(packageJsonStub));
+  });
+  it('should generate the expected files and configuration for a grid component', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = await runner.runSchematic(
       'ng-ag-grid-schematics',
-      { method: '' },
-      Tree.empty()
+      {
+        name: 'grid',
+        path: '',
+        project: 'test-project',
+        style: 'css',
+        state: 'no-state'
+      },
+      testTree
     );
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual([
+      '/angular.json',
+      '/package.json',
+      '/src/app/grid/grid.component.css',
+      '/src/app/grid/grid.component.spec.ts',
+      '/src/app/grid/grid.component.ts'
+    ]);
   });
 });
